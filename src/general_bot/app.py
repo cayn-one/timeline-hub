3597e5128c9ef0e0d2075a7ed3cc20bfeb903c97
whiplash-0 <eenvalee@gmail.com>
2026-03-04 13:45:14 +0100

    chore: add telegram bot base and config

--- src/general_bot/app.py
+++ src/general_bot/app.py
@@ -1,2 +1,19 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from general_bot import handlers
+from general_bot.config import config
+
+
 def run() -> None:
-    ...
+    asyncio.run(_main())
+
+
+async def _main() -> None:
+    bot = Bot(config.bot_token)
+    dp = Dispatcher()
+    dp.include_router(handlers.router)
+
+    async with bot:
+        await dp.start_polling(bot, polling_timeout=30)
