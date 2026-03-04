22a8582dfa6e17f5feba167b13c212f645c1232e
whiplash-0 <eenvalee@gmail.com>
2026-03-04 22:54:09 +0100

    chore: add type hints to `enforce_allowlist` middleware function

--- src/general_bot/app.py
+++ src/general_bot/app.py
@@ -2,10 +2,11 @@ import asyncio
 from typing import Any
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import User
+from aiogram.types import Update, User
 
 from general_bot import handlers
 from general_bot.config import config
+from general_bot.types import Handler, MiddlewareData
 
 
 def run() -> None:
@@ -17,11 +18,11 @@ async def _main() -> None:
     dp.include_router(handlers.router)
 
     @dp.update.middleware()
-    async def enforce_allowlist(handler, event, data) -> Any:
+    async def enforce_allowlist(handler: Handler, update: Update, data: MiddlewareData) -> Any:
         user: User | None = data.get('event_from_user')
         if user is None or user.id not in config.allowlist:
             return None
-        return await handler(event, data)
+        return await handler(update, data)
 
     async with Bot(config.bot_token) as bot:
         await dp.start_polling(bot, polling_timeout=30)
---
+++ src/general_bot/types.py
@@ -0,0 +1,7 @@
+from collections.abc import Awaitable
+from typing import Any, Callable
+
+from aiogram.types import TelegramObject
+
+MiddlewareData = dict[str, Any]
+Handler = Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]]
